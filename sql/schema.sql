e6365e0397d202a764b98ad3f3758daf0c68f449
Jabah Gundy <gundy157@outlook.com>
2021-07-07 12:44:31 -0400

    Updated Project

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,18 @@
+CREATE TABLE rankings (
+    id serial PRIMARY KEY,
+    movie_rankings text NOT NULL
+); 
+
+CREATE TABLE movies (
+    id serial PRIMARY KEY,
+    movie_title text NOT NULL,
+    movie_id integer REFERENCES rankings(id) 
+);
+
 CREATE TABLE users (
-    id serial PRIMARY KEY, 
-    first_name text,
-    last_name text, 
+    id serial PRIMARY KEY,
+    first_name text, 
+    last_name text,
     email varchar(200),
     password varchar(2000)
-); 
+);
